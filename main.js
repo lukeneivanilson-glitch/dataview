9241d245c318841e403fc0d83155ef9ea34e816a
Delgado-Sly <delgadosly2006@gmail.com>
2026-02-09 07:40:18 +0100

    Update index.html - Removed the main hub section from index.html for a cleaner layout. - Added a new main.js file containing a simple counter and array iteration logic.

---
+++ main.js
@@ -0,0 +1,12 @@
+let counter = 0;
+const target = 10;
+const arr = [1,4,7,10];
+
+if (counter ==0){
+    while(counter <5){
+        arr.forEach(item => {
+            console.log(item);
+        });
+    }
+
+}
